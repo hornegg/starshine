4e19b186ca9f297c5bd0bc70ac241c4287ee8f9f
hornegg <hornegg@users.noreply.github.com>
2020-11-22 15:22:24 +0000

    Port original starshine sketch over to p5js (headless).

--- starshine-js/starshine.ts
+++ starshine-js/starshine.ts
@@ -16,12 +16,34 @@ new p5((p: p5) => {
     fs.writeFileSync(filename, buffer);
   };
 
+  const star = (x: number, y: number, radius: number, alpha: number) => {
+    p.push();
+    p.stroke(255, alpha);
+    p.fill(255, alpha);
+    p.translate(x, y);
+    p.beginShape();
+    p.rotate(p.HALF_PI);
+    for (let angle = 0; angle <= 4*p.PI; angle += (4*p.PI) / 5) {
+      p.vertex(radius * p.cos(angle), radius * p.sin(angle));   
+    }
+    p.endShape();
+    p.pop();
+  };
+  
   p.setup = () => {
-    p.createCanvas(100, 100);
-    p.background(255, 128, 64);
-    p.rect(50, 50, 25, 25);
+    p.createCanvas(512, 800);
+    p.background(0);
 
-    writeGraphicsToPng(p, 'out.png');
+    for (let t = 0; t <= 1; t += 0.01) {
+      star(
+        p.map(t, 0, 1, p.width * 0.4, p.width * 0.6),
+        p.map(t, 0, 1, p.height * 0.3, p.height * 0.7),
+        p.map(t, 0, 1, 60, 120),
+        p.map(p.pow(t, 0.1), 0, 1, 255, 0)
+      );
+    }
+  
+    writeGraphicsToPng(p, 'starshine.png');
     process.exit(0);
   };
 });
